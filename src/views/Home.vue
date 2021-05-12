03ac379e66504ff4c5dfb073fb94f4672b994263
Jorge Callisaya <sanchezenrique580@gmail.com>
2021-05-11 21:11:38 -0400

    Creacion de calendario con fechas maximas y minimas de acuerdo a la API

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,15 +1,36 @@
 <template>
-  <hello-world />
+<div>
+<v-layout :wrap="true">  <!-- ROW -->
+  <v-flex xs12> <!-- COLUMN -->
+  <v-card>
+    <v-date-picker v-model="fecha"
+    full-width
+    locale="es-bo"
+    :min="minimo"
+    :max="maximo"
+    ></v-date-picker>
+  </v-card>
+  <v-card dark color="error">
+    <v-card-text class="display-1 text-center">6.98 - {{fecha}}</v-card-text>
+  </v-card>
+  </v-flex>
+ 
+</v-layout>
+</div>
 </template>
 
 <script>
-  import HelloWorld from '../components/HelloWorld'
 
   export default {
     name: 'Home',
-
     components: {
-      HelloWorld,
     },
+    data () {
+      return {
+        fecha: '',
+        minimo:'1984',
+        maximo: new Date().toISOString().substr(0, 10).replace(new Date().getDate()+1,new Date().getDate())
+      }
+      }
   }
 </script>
