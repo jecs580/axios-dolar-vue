01e654bf35defaac75e89ac2fb81c8f9abaaad19
Jorge Callisaya <sanchezenrique580@gmail.com>
2021-05-12 21:34:25 -0400

    Usando la API de mindicador.cl para obtener los datos de conversion y mostrarlos por cada fecha seleccionada

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -8,10 +8,11 @@
     locale="es-bo"
     :min="minimo"
     :max="maximo"
+    @change="getDolar(fecha)"
     ></v-date-picker>
   </v-card>
   <v-card dark color="error">
-    <v-card-text class="display-1 text-center">6.98 - {{fecha}}</v-card-text>
+    <v-card-text class="display-1 text-center">{{valor? valor:'NaN'}}</v-card-text>
   </v-card>
   </v-flex>
  
@@ -20,17 +21,38 @@
 </template>
 
 <script>
-
+  import axios from 'axios'
   export default {
     name: 'Home',
     components: {
     },
     data () {
       return {
-        fecha: '',
+        fecha: new Date().toISOString().substr(0, 10).replace(new Date().getDate()+1,new Date().getDate()),
         minimo:'1984',
-        maximo: new Date().toISOString().substr(0, 10).replace(new Date().getDate()+1,new Date().getDate())
+        maximo: new Date().toISOString().substr(0, 10).replace(new Date().getDate()+1,new Date().getDate()),
+        valor:null
       }
+      },
+      methods:{
+        async getDolar(dia){
+          let arrayFecha= dia.split(['-']);
+          arrayFecha.reverse();
+          let fecha =  arrayFecha.join('-')
+          try {
+            let datos = await axios.get(`https://mindicador.cl/api/dolar/${fecha}`);
+            if(datos.data.serie[0]!== undefined){
+              this.valor = datos.data.serie[0].valor;
+            }else{
+              this.valor= null;
+            }
+          } catch (error) {
+            console.log(error);
+          }
+        }
+      },
+      created(){
+        this.getDolar(this.fecha);
       }
   }
 </script>
